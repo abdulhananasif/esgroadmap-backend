d1af4426c1a07e87d0a8bd3f4a584e4d9a5cf8f5
Abdul Hanan Asif <abdulhananasif5643@gmail.com>
2025-04-20 17:04:29 +0500

    build files updated

--- api/server.js
+++ api/server.js
@@ -3,11 +3,23 @@ import dotenv from 'dotenv';
 import prepareV1Routes from './apiVersion/v1/index.js';
 import { PrismaClient } from './generated/prisma/index.js';
 import cookieParser from 'cookie-parser';
+import cors from 'cors';
 export const prisma = new PrismaClient();
 dotenv.config();
 const app = express();
 app.use(express.json());
 app.use(cookieParser());
+const allowedorigins = [
+    'https://esgroadmap-frontend.vercel.app/',
+    'http://localhost:3000',
+];
+app.use(cors({
+    origin: allowedorigins,
+    methods: 'GET,PUT,POST,DELETE,PATCH,HEAD',
+    preflightContinue: false,
+    optionsSuccessStatus: 204,
+    credentials: true,
+}));
 prepareV1Routes(app);
 const port = process.env.PORT || 5000;
 app.get('/', (req, res) => {
