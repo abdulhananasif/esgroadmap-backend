f9d10ca993cbb5e4697a76ea7fe5d626765c45cc
Mehmood Akmal <akmalbajwa2672@gmail.com>
2025-04-21 12:08:48 +0500

    origine reset in cors

--- src/server.ts
+++ src/server.ts
@@ -13,9 +13,7 @@ app.use(express.json());
 app.use(cookieParser());
 
 const allowedorigins = [
-  '*',
   'https://esgroadmap-frontend.vercel.app',
-  'http://localhost:3000',
   'http://localhost:5173',
 ];
 app.use(
