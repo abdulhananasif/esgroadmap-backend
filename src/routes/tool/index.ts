167c55ec0aff0b8a201b1440a4e5a46c792b2d78
Mehmood Akmal <akmalbajwa2672@gmail.com>
2025-04-24 15:37:06 +0500

    carbon reduction api created

---
+++ src/routes/tool/index.ts
@@ -0,0 +1,13 @@
+import express from 'express';
+import authenticateUser from '../../middleware/authenticateUser.js';
+import {carbonReduction} from '../../controller/tool/index.js';
+
+const toolRouter = express.Router();
+
+toolRouter.get(
+  '/carbonReduction',
+  authenticateUser as unknown as express.RequestHandler,
+  carbonReduction as unknown as express.RequestHandler
+);
+
+export default toolRouter;
