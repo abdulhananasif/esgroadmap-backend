61c568a5917420934e85c5cde48df126df7ac656
Abdul Hanan Asif <abdulhananasif5643@gmail.com>
2025-04-24 16:26:08 +0500

    middleware removecx

--- src/routes/tool/index.ts
+++ src/routes/tool/index.ts
@@ -6,7 +6,7 @@ const toolRouter = express.Router();
 
 toolRouter.get(
   '/carbonReduction',
-  authenticateUser as unknown as express.RequestHandler,
+  // authenticateUser as unknown as express.RequestHandler,
   carbonReduction as unknown as express.RequestHandler
 );
 
