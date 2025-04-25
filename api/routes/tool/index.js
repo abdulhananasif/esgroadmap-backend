584520f0681862e48892226b8a58f80036659d47
Abdul Hanan Asif <abdulhananasif5643@gmail.com>
2025-04-25 11:45:10 +0500

    build file updated

--- api/routes/tool/index.js
+++ api/routes/tool/index.js
@@ -1,7 +1,22 @@
 import express from 'express';
-import { carbonReduction } from '../../controller/tool/index.js';
+import { carbonReduction, renewables, sentenceGender, supplyChain, wasteAndRecycling, waterManagement, } from '../../controller/tool/index.js';
 const toolRouter = express.Router();
 toolRouter.get('/carbonReduction', 
 // authenticateUser as unknown as express.RequestHandler,
 carbonReduction);
+toolRouter.get('/wasteAndRecycling', 
+// authenticateUser as unknown as express.RequestHandler,
+wasteAndRecycling);
+toolRouter.get('/waterManagement', 
+// authenticateUser as unknown as express.RequestHandler,
+waterManagement);
+toolRouter.get('/sentenceGender', 
+// authenticateUser as unknown as express.RequestHandler,
+sentenceGender);
+toolRouter.get('/supplyChain', 
+// authenticateUser as unknown as express.RequestHandler,
+supplyChain);
+toolRouter.get('/renewables', 
+// authenticateUser as unknown as express.RequestHandler,
+renewables);
 export default toolRouter;
