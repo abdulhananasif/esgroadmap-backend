674b282e81c7558e788bd9edb332b0f5379de3fc
Mehmood Akmal <akmalbajwa2672@gmail.com>
2025-05-16 12:33:20 +0500

    Build created

--- api/controller/auth/index.js
+++ api/controller/auth/index.js
@@ -94,15 +94,10 @@ export const regenerateToken = async (req, res) => {
         return;
     }
     catch (accessErr) {
-        if (accessErr.name !== 'TokenExpiredError') {
-            console.error('Invalid access token:', accessErr.message);
-            res.status(401).json({ message: 'Invalid access token' });
-            return;
-        }
         try {
             const decodedRefresh = jwt.verify(refreshToken, refresh);
             const newAccessToken = generateAccessToken(decodedRefresh.id);
-            const newRefreshToken = generateAccessToken(decodedRefresh.id);
+            const newRefreshToken = generateRefreshToken(decodedRefresh.id);
             setAuthCookies(res, newAccessToken, newRefreshToken);
             console.log('New tokens generated');
             res.status(200).json({ message: 'Access and Refresh tokens are re-generated' });
