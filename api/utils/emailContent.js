16d9a56e6333a3de46f44fb0aec9970c731df08a
Abdul Hanan Asif <abdulhananasif5643@gmail.com>
2025-05-16 16:30:23 +0500

    url changed

--- api/utils/emailContent.js
+++ api/utils/emailContent.js
@@ -1,5 +1,5 @@
 export const emailContent = `
 <p>Hello Dear</p>
 <p>Thank you for signing up! Please click the button below to activate your account:</p>
-<a href=${process.env.CLIENT_URL}>Activate Account</a>
+<a href=${process.env.CLIENT_URL}/auth/activate-account>Activate Account</a>
 `;
