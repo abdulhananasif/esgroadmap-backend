3f650c160ef16d57c2d53124ccd73b77b3e3ecc8
Mehmood Akmal <akmalbajwa2672@gmail.com>
2025-06-18 16:19:19 +0500

    build updated

--- api/controller/payment/index.js
+++ api/controller/payment/index.js
@@ -2,7 +2,9 @@ import { paddle } from '../../utils/paddleClient.js';
 export const products = async (req, res) => {
     try {
         const response = await paddle.get('/products');
+        console.log("🚀 ~ products ~ response:", response);
         const prices = await paddle.get('/prices');
+        console.log("🚀 ~ products ~ prices:", prices);
         res.status(200).json({ product: response.data, prices: prices.data });
     }
     catch (error) {
