e145b6544e2b763f2ba2d829213dd327ff621656
Mehmood Akmal <akmalbajwa2672@gmail.com>
2025-06-18 16:18:46 +0500

    add logs

--- src/controller/payment/index.ts
+++ src/controller/payment/index.ts
@@ -5,7 +5,9 @@ import { paddle } from '../../utils/paddleClient.js';
 export const products = async (req: Request, res: Response) => {
   try {
     const response = await paddle.get('/products');
+    console.log("🚀 ~ products ~ response:", response)
     const prices = await paddle.get('/prices');
+    console.log("🚀 ~ products ~ prices:", prices)
     res.status(200).json({product: response.data , prices: prices.data});
   } catch (error: any) {
     console.error('Error fetching Paddle products:', error);
--- src/utils/paddleClient.ts
+++ src/utils/paddleClient.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const paddle = axios.create({
-    baseURL: 'https://api.paddle.com',
+    baseURL: 'https://sandbox-api.paddle.com',
     headers: {
         authorization: `Bearer ${process.env.PADDLE_API_KEY}`,
         'Content-Type' : 'application/json'
