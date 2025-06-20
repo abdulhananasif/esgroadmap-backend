53d8cf17740a31887f1abc61e33069d14a38e873
Mehmood Akmal <akmalbajwa2672@gmail.com>
2025-06-20 21:00:54 +0500

    corse add

--- src/server.ts
+++ src/server.ts
@@ -15,6 +15,7 @@ app.use(cookieParser());
 
 const allowedorigins = [
   'https://esgroadmap-frontend.vercel.app',
+  'https://esgroadmap-fe-lemon.vercel.app',
   'http://localhost:5173',
 ];
 app.use(
