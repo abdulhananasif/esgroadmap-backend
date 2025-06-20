1532a268e03aefe1706545a56495bbb3aa463106
Mehmood Akmal <akmalbajwa2672@gmail.com>
2025-06-20 20:13:41 +0500

    paddle products

--- src/controller/payment/index.ts
+++ src/controller/payment/index.ts
@@ -18,12 +18,9 @@ interface PaddleResponse {
 export const products = async (req: Request, res: Response) => {
   try {
     const response = await paddle.get('/products');
-    console.log("🚀 ~ products ~ response:", response)
     const prices = await paddle.get('/prices');
-    console.log("🚀 ~ products ~ prices:", prices)
-        const subscription = await paddle.get('/subscriptions');
 
-    res.status(200).json({product: response.data , prices: prices.data , subscription: subscription.data});
+    res.status(200).json({product: response.data , prices: prices.data});
   } catch (error: any) {
     console.error('Error fetching Paddle products:', error);
     res.status(500).json({ message: 'Failed to fetch products', error: error.message });
